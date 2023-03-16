a527a31ee2e8f2a29752aae6d1f5f75c9a6b5c05
cuongnt97 <cuong97ndc@gmail.com>
2023-03-16 18:49:55 +0700

    commit

--- Class7/cara-watch-react/vite.config.js
+++ Class7/cara-watch-react/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./Class7/cara-watch-react",
+  base: "/Class7/cara-watch-react",
   plugins: [react()],
   build: {
     outDir: "dist",
